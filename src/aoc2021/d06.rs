1c466c598887099b2116b2ec6e8cd7b7810c937a
ostracised3rd <eivazy.soheil@gmail.com>
2021-12-06 22:21:04 +0330

    2021 day 6 revisited

--- src/aoc2021/d06.rs
+++ src/aoc2021/d06.rs
@@ -5,16 +5,16 @@ use crate::helpers::load_data;
 pub fn run() {
     let data = load_data("data/aoc2021/d06.txt");
     let data = data.split(",").map(|x| x.parse().unwrap()).collect::<Vec<usize>>();
-    let res = thats_a_rotate(18, data);
+    let res = thats_a_rotate(18, &data);
     println!("{}", res);
 }
 
 
-fn thats_a_rotate(days: u32, data: Vec<usize>) -> i64 {
+fn thats_a_rotate(days: u32, data: &Vec<usize>) -> i64 {
     let mut gens = Vec::<i64>::from([0; 9]);
     
     for i in data {
-        gens[i] += 1;
+        gens[*i] += 1;
     }
 
     for i in 0..days {
@@ -35,8 +35,8 @@ mod tests {
     fn generations() {
         let data: Vec<usize> = vec![3,4,3,1,2];
 
-        assert_eq!(26, thats_a_rotate(18, data.clone())); 
-        assert_eq!(5934, thats_a_rotate(80, data.clone()));
-        assert_eq!(26984457539, thats_a_rotate(256, data));
+        assert_eq!(26, thats_a_rotate(18, &data)); 
+        assert_eq!(5934, thats_a_rotate(80, &data));
+        assert_eq!(26984457539, thats_a_rotate(256, &data));
     }
 }
